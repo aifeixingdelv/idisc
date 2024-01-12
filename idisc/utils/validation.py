8fc5edcb68cc750e3e1149ad348a7ed2d5e37f45
aifeixingdelv <67185045+aifeixingdelv@users.noreply.github.com>
2024-01-12 13:37:18 +0800

    Modify files

--- idisc/utils/validation.py
+++ idisc/utils/validation.py
@@ -89,8 +89,7 @@ def validate(
 
         depths = [colorize(p.squeeze().cpu().numpy(), 0, 80) for p in preds]
         for index, depth in enumerate(depths):
-            save_name = os.path.join("outputs", f"batch{i}_{index}.png")
-            Image.fromarray(depth).save(save_name)
+            Image.fromarray(depth).save(f"outputs/batch{i}_{index}.png")
         losses = {k: v for l in losses.values() for k, v in l.items()}
         for loss_name, loss_val in losses.items():
             ds_losses[loss_name] = (
